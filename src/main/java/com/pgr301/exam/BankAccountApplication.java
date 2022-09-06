0364df59b10799d5397f55ef8182076b17c67c17
EC2 Default User <ec2-user@ip-172-31-39-249.eu-west-1.compute.internal>
2022-09-06 09:58:49 +0000

    compilation error introduced

--- src/main/java/com/pgr301/exam/BankAccountApplication.java
+++ src/main/java/com/pgr301/exam/BankAccountApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BankAccountApplication {
-    public static void mai(String[] args) {
+    public static void main(String[] args) {
         SpringApplication.run(BankAccountApplication.class, args);
     }
 }
